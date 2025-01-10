d7c99682d9bcc3caae3f2ccb8b2a3fca14077ef9
GulnazNurseit <gulnaznurseit1227@gmail.com>
2025-01-10 19:35:12 +0500

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -2,19 +2,19 @@ public class Employee extends Person {
     private String position;
     private double salary;
 
-    // Default Constructor
+  
     public Employee() {
         super();
     }
 
-    // Parameterized Constructor
+  
     public Employee(String name, String surname, String position, double salary) {
         super(name, surname);
         this.position = position;
         this.salary = salary;
     }
 
-    // Getters and Setters
+  
     public String getPosition() {
         return position;
     }
@@ -31,13 +31,13 @@ public class Employee extends Person {
         this.salary = salary;
     }
 
-    // toString Method
+   
     @Override
     public String toString() {
         return "Employee: " + super.toString();
     }
 
-    // Payable Method
+   
     @Override
     public double getPaymentAmount() {
         return salary;
