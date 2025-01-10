a27d5f9c18a68e1706ae8d423cd0c090edbdddb2
GulnazNurseit <gulnaznurseit1227@gmail.com>
2025-01-10 19:33:56 +0500

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -4,19 +4,19 @@ public abstract class Person implements Payable, Comparable<Person> {
     private String name;
     private String surname;
 
-    // Default Constructor
+   
     public Person() {
         this.id = counter++;
     }
 
-    // Parameterized Constructor
+    
     public Person(String name, String surname) {
         this.id = counter++;
         this.name = name;
         this.surname = surname;
     }
 
-    // Getters and Setters
+ 
     public int getId() {
         return id;
     }
@@ -43,10 +43,10 @@ public abstract class Person implements Payable, Comparable<Person> {
         return id + ". " + name + " " + surname;
     }
 
-    // Abstract Method
+  
     public abstract String getPosition();
 
-    // Implementing compareTo Method
+    
     @Override
     public int compareTo(Person other) {
         return Double.compare(this.getPaymentAmount(), other.getPaymentAmount());
